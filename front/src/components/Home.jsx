311ed6e60eb6e995d9a9b4fcd53198ab325c452d
romainniamor <romainnavoret@gmail.com>
2024-06-03 23:24:24 +0200

    🔧 First Commit

---
+++ front/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Home() {
+  return <HomeStyled>Home</HomeStyled>;
+}
+
+const HomeStyled = styled.div`
+  color: red;
+`;
