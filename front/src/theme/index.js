d1c34f2b7f0b424db571554754ef6510030de8e5
romainniamor <romainnavoret@gmail.com>
2024-06-09 11:21:04 +0200

    💄 Create design system

---
+++ front/src/theme/index.js
@@ -0,0 +1,60 @@
+const colors = {
+  dark: "#17161a",
+  light: "#ffffff",
+  background_white: "#f5f5f7",
+  background_dark: "#292729",
+  primary: "#f3c4bb",
+  greyLight: "#e4e5e9",
+  greyMedium: "#a7a8ad",
+  greySemiDark: "#93a2b1",
+  greyDark: "#6f737e",
+};
+
+const spacing = {
+  xxs: "4px",
+  xs: "8px",
+  sm: "12px",
+  s: "15px",
+  md: "20px",
+  lg: "32px",
+  xl: "52px",
+  xxl: "84px",
+};
+
+const fonts = {
+  xxxs: "6px",
+  xxs: "8px",
+  xs: "10px",
+  s: "12px",
+  sm: "15px",
+  P0: "16px",
+  P1: "18px",
+  P2: "20px",
+  P3: "24px",
+  P4: "36px",
+  P5: "48px",
+  P6: "60px",
+};
+
+const borderRadius = {
+  round: "5px",
+  extraRound: "15px",
+  circle: "50%",
+};
+
+const weights = {
+  light: 300,
+  regular: 400,
+  medium: 500,
+  semiBold: 600,
+  bold: 700,
+  heavy: 800,
+};
+
+export const theme = {
+  colors,
+  fonts,
+  borderRadius,
+  weights,
+  spacing,
+};
