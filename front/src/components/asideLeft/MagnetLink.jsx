7105907b1c5c4eaa5287b5a29c06877d0ca4428c
romainniamor <romainnavoret@gmail.com>
2024-06-10 22:56:56 +0200

    🚧 Modify MagneticLink

--- front/src/components/asideLeft/MagnetLink.jsx
+++ front/src/components/asideLeft/MagnetLink.jsx
@@ -1,9 +1,8 @@
 import { useRef, useState, useEffect } from "react";
 import { gsap } from "gsap";
 import styled from "styled-components";
-import { AiOutlineGithub } from "react-icons/ai";
 
-export default function MagneticLink({ url }) {
+export default function MagneticLink({ children }) {
   const ref = useRef(null);
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
@@ -14,7 +13,7 @@ export default function MagneticLink({ url }) {
     const { height, width, left, top } = ref.current.getBoundingClientRect();
     const middleX = clientX - (left + width / 2);
     const middleY = clientY - (top + height / 2);
-    setPosition({ x: middleX, y: middleY });
+    setPosition({ x: middleX * 2, y: middleY * 2 });
   };
 
   const reset = () => {
@@ -38,23 +37,17 @@ export default function MagneticLink({ url }) {
       onMouseMove={handleMouse}
       onMouseLeave={reset}
     >
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        <AiOutlineGithub size={50} />
-      </a>
+      {children}
     </MagneticLinkStyled>
   );
 }
 
 const MagneticLinkStyled = styled.div`
+  cursor: pointer;
   position: relative;
   padding: 10px;
-  background: #000;
   border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
-
-  a {
-    display: flex;
-  }
 `;
