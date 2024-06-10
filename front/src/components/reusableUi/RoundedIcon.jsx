8a4095a72e929f0705af84c823f70b58b73c9eab
romainniamor <romainnavoret@gmail.com>
2024-06-10 22:56:04 +0200

    🚧 Create reusable component RoundedIcon

---
+++ front/src/components/reusableUi/RoundedIcon.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../../theme/index";
+
+export default function RoundedIcon({ url, icon }) {
+  return (
+    <RoundedIconStyled>
+      <a href={url}>{icon}</a>
+    </RoundedIconStyled>
+  );
+}
+
+const RoundedIconStyled = styled.div`
+  cursor: pointer;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  a {
+    font-size: 50px;
+    transition: all 0.3s ease-in-out;
+    color: ${theme.colors.greyDark};
+    &:hover {
+      transform: scale(1.05);
+      color: ${theme.colors.greyLight};
+    }
+    &:active {
+      transform: scale(1);
+      color: ${theme.colors.greyDark};
+    }
+  }
+`;
