fcc7e731cc76c2aae713afb5526d4b19d849b4d6
romainniamor <romainnavoret@gmail.com>
2024-06-10 22:57:52 +0200

    Config app

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import Home from "./components/Home";
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
