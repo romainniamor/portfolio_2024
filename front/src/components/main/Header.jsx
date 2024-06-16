08033a79a03527852baed031d3af2f725f2472f1
romainniamor <romainnavoret@gmail.com>
2024-06-16 18:37:19 +0200

    🚧 Create header and overlay

--- front/src/components/main/Header.jsx
+++ front/src/components/main/Header.jsx
@@ -1,18 +1,40 @@
 import React from "react";
 import styled from "styled-components";
+import { theme } from "../../theme";
 
 export default function Header() {
   return (
     <HeaderStyled>
-      <h1>Header</h1>
+      <section>
+        <h1>Header</h1>
+      </section>
+      <section className="overlay">
+        <h1>Header</h1>
+      </section>
     </HeaderStyled>
   );
 }
 
 const HeaderStyled = styled.div`
-  flex: 1;
-  background-color: pink;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  color: ${theme.colors.greyLight};
+  section {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &.overlay {
+      background-color: rgb(163 230 53);
+      color: ${theme.colors.dark};
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      width: 100vw;
+      height: 100vh;
+
+      clip-path: circle(170px at 50% 50%);
+    }
+  }
 `;
