29e00cdc1f6ce430c01fb3b7c37964e42c048530
romainniamor <romainnavoret@gmail.com>
2024-06-17 23:13:51 +0200

    🚧 Create context

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -3,14 +3,19 @@ import styled from "styled-components";
 import Footer from "./footer/Footer";
 import Header from "./main/Header";
 import NavBar from "./nav/NavBar";
+import AppContext from "../contexts/appContext";
 
 export default function Home() {
+  const appContextValue = {};
+
   return (
-    <HomeStyled>
-      <NavBar />
-      <Header />
-      <Footer />
-    </HomeStyled>
+    <AppContext.Provider value={appContextValue}>
+      <HomeStyled>
+        <NavBar />
+        <Header />
+        <Footer />
+      </HomeStyled>
+    </AppContext.Provider>
   );
 }
 
